3a79c8a3c1b49c79bb3f831e609bf5f1f20d9037
Dominik Moreira <dominik.privatacc@gmail.com>
2026-04-20 22:36:41 +0200

    Initial Commit

---
+++ MultiMap/MultiMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiMapApp.swift
+//  MultiMap
+//
+//  Created by Dominik de Jesus Moreira on 20.04.26.
+//
+
+import SwiftUI
+
+@main
+struct MultiMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
